9b9361120ba6db973f7626e4d9aa38323c2e2367
Kamimu-672 <mwanichris@gmail.com>
2024-04-23 11:26:48 +0000

    ES6 Basics

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,15 @@
-export function taskFirst () {
+export function taskFirst() {
+  // Use const as the value won't change
   const task = 'I prefer const when I can.';
   return task;
 }
 
-export function getLast () {
+export function getLast() {
   return ' is okay';
 }
 
-export function taskNext () {
+export function taskNext() {
+  // Use let as the value is being modified
   let combination = 'But sometimes let';
   combination += getLast();
 
